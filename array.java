5ebc164de221ecad3841619c8980a4014887bfbf
harshit2002-web <haneja111@gmail.com>
2025-01-24 14:51:27 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,13 @@
+public class array {
+    public static void main(String[] args) {
+        int[] arr = new int [5];
+        arr[0]=12;
+        arr[1]=13;
+        arr[2]=14;
+        arr[3]=15;
+        arr[4]=19;
+
+        for (int i=0; i<arr.length; i++ )
+        System.out.println(arr[i]);
+    }
+}
